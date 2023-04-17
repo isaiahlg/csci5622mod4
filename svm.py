4ba2b285d8d9bbea0b6714ee952504e76de0a3a8
Isaiah Lyons-Galante <ilyonsg@nrel.gov>
2023-04-16 19:11:00 -0600

    Generate output figures, finalize code.

--- svm.py
+++ svm.py
@@ -1,22 +1,22 @@
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 import pandas as pd
 import numpy as np
 import matplotlib as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 import seaborn as sns
 import os as os
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-#%% -----------------------------------
 # get current working directory
 wd = os.getcwd()
 
-# read in entire survey
+#%% -----------------------------------------------------------------
+# READ IN SURVEY DATA
 # for surface
 # sl19 = pd.read_stata("~/Documents/code/csci5622/csci5622mod4/proj/data/SLHR7ADT/SLHR7AFL.DTA")
 # for PC
@@ -25,25 +25,29 @@ wd = os.getcwd()
 # generalized
 sl19 = pd.read_stata(wd + "/data/SLHR7ADT/SLHR7AFL.DTA")
 
-#%% -----------------------------------
-# keep just columns of interest
+#%% -----------------------------------------------------------------
+# FILTER DOWN
+# keep just columns of interest and rexport
 cols2keep = ["hv000","hv001","hv006","hv007","hv010","hv011","hv012","hv013","hv014","hv024","hv025","hv040","hv045c","hv201","hv204","hv205","hv206","hv207","hv208","hv209","hv210","hv211","hv212","hv213","hv214","hv215","hv216","hv217","hv219","hv220","hv221","hv226","hv227","hv230a","hv237","hv241","hv243a","hv243b","hv243c","hv243d","hv243e","hv244","hv245","hv246","hv246a","hv246b","hv246c","hv246d","hv246e","hv246f","hv247","hv270","hv271","hv270a","hv271a","hml1"]
 sl19keep = sl19[cols2keep]
 
-#%% -----------------------------------
+# export csv
+sl19keep.to_csv(wd + "/data/sl19keep.csv", index=False)
+
 # keep just numeric variables of interest
 intCols = ["hv010","hv011","hv012","hv014","hv216","hv270"]
 sl19num = sl19[intCols]
+sl19num["hv270"]=pd.factorize(sl19num["hv270"])[0]
 
 # export csv
-sl19num.to_csv(wd + "/data/sl19svm.csv")
-
-# copy to prepare for cleaning
-df = sl19num
-df["hv270"]=pd.factorize(df["hv270"])[0]
+sl19num.to_csv(wd + "/data/sl19svm.csv", index=False)
+print("Exported!")
+#%% ------------------------------------------------------------------
+# read in data to prepare for cleaning
+df = pd.read_csv(wd + "/data/sl19svm.csv")
 df.describe()
 
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 # convert categorical
 # df.loc[df["hv245"] == "don't know", "hv245"] = 10
 # df.loc[df["hv245"] == "unknown", "hv245"] = 10
@@ -51,7 +55,7 @@ df.describe()
 # df["hv245"]=pd.factorize(df["hv245"])[0]
 # df.loc[df["hv245"] == -1, "hv245"] = 10
 
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 # scale data with mean = 0, stddev = 1
 scaler = StandardScaler()
 sl19scaled = scaler.fit_transform(df)
@@ -64,7 +68,7 @@ stddevs = scaler.scale_
 sl19scaledDf = pd.DataFrame(sl19scaled)
 sl19scaledDf.to_csv(wd + "/data/sl19scaled.csv")
 
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 # reduce columns down to 2 for clustering
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(sl19scaled)
@@ -76,7 +80,7 @@ finalDf = pd.concat([principalDf, sl19[[z]]], axis = 1)
 sns.set(rc={'figure.figsize':(8,10)})
 sns.relplot(data=finalDf, x="pc1", y="pc2", hue=z, size=0.5).set(title="2 Principal Components of Numerical Data by Wealth Index")
 
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 # split test and train data
 df_train, df_test = train_test_split(df, test_size=0.2)
 
@@ -86,10 +90,11 @@ df_test_nolabels = df_test.drop(["hv270"], axis=1)
 labels_train = df_train["hv270"]
 df_train_nolabels = df_train.drop(["hv270"], axis=1)
 
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 ## LINEAR KERNEL
 # fit SVM model
-svm_model1=LinearSVC(C=1)
+c1 = 1
+svm_model1=LinearSVC(C=c1)
 svm_model1.fit(df_train_nolabels, labels_train)
 
 # predict test data
@@ -105,13 +110,12 @@ print(accuracy_1)
 ax1 = sns.heatmap(cf_matrix_1/np.sum(cf_matrix_1), annot=True, fmt='.1%')
 ax1.set_xlabel("Predicted Classes")
 ax1.set_ylabel("Actual Classes")
-ax1.set_title("Confusion Matrix for Linear Kernel, C=1")
+ax1.set_title("Confusion Matrix for Linear Kernel, C="+str(c1))
 ax1.text(2,6, "Accuracy: "+str(round(accuracy_1*100, 1))+"%")
-# %%
-
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 ## RBF KERNEL
-svm_model2=SVC(C=1.0, kernel='rbf', degree=3, gamma="auto")
+c2 = 100
+svm_model2=SVC(C=c2, kernel='rbf', degree=3, gamma="auto")
 svm_model2.fit(df_train_nolabels, labels_train)
 
 # predict test data
@@ -127,12 +131,13 @@ print(accuracy_2)
 ax2 = sns.heatmap(cf_matrix_2/np.sum(cf_matrix_2), annot=True, fmt='.1%')
 ax2.set_xlabel("Predicted Classes")
 ax2.set_ylabel("Actual Classes")
-ax2.set_title("Confusion Matrix for RBF Kernel, C=1")
+ax2.set_title("Confusion Matrix for RBF Kernel, C="+str(c2))
 ax2.text(2,6, "Accuracy: "+str(round(accuracy_2*100, 1))+"%")
 
-#%% -----------------------------------
+#%% -----------------------------------------------------------------
 ## POLYNOMIAL KERNEL
-svm_model3=SVC(C=1.0, kernel='poly', degree=2, gamma="auto")
+c3 = 100 
+svm_model3=SVC(C=c3, kernel='poly', degree=2, gamma="auto")
 svm_model3.fit(df_train_nolabels, labels_train)
 
 # predict test data
@@ -148,5 +153,5 @@ print(accuracy_3)
 ax3 = sns.heatmap(cf_matrix_3/np.sum(cf_matrix_3), annot=True, fmt='.1%')
 ax3.set_xlabel("Predicted Classes")
 ax3.set_ylabel("Actual Classes")
-ax3.set_title("Confusion Matrix for Polynomial Kernel, C=1")
+ax3.set_title("Confusion Matrix for Polynomial Kernel, C="+str(c3))
 ax3.text(2,6, "Accuracy: "+str(round(accuracy_3*100, 1))+"%")
